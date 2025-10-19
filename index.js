77598314e633eb6f7606088e79d16b02d29b4aa6
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-19 12:11:02 +0600

    changed port: 3000 to 5000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
